3b1c50b9ffcb935cfb4f8c83aeb46eb182c768c7
Kalin Todorov <80665676+callinGudman@users.noreply.github.com>
2021-06-09 14:50:28 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+'use strict'
+let navLinks =document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right="-20rem";
+}
+
+//Video
+const head = document.querySelectorAll('.head-col,.bottomSecond, .bottomFirst, .bottomAlone,.videoMob,.videoCv,.videoCvBB,.videoCvB');
+const stopVideo = document.querySelector('.stop-btn');
+const testing = document.querySelector('.testing');
+const openVideo = function (){
+    testing.classList.remove('hidden');
+    stopVideo.classList.remove('hidden');
+}
+const closeVideo = function (){
+    testing.classList.add('hidden');
+    stopVideo.classList.add('hidden');
+}
+for(let i = 0; i < head.length;i++){
+    head[i].addEventListener('click',openVideo)
+}
+stopVideo.addEventListener('click', closeVideo);
+document.addEventListener('keydown', function (e){
+    if(e.key === 'Escape' && !testing.classList.contains('hidden')){
+        closeVideo();
+    }
+})
+
